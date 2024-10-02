6ce7dd9d9d525df188f660d560a215ccfc35a82f
Zzrispu <122392767+Zzrispu@users.noreply.github.com>
2024-10-02 11:46:58 -0300

    Update settings.py
    secrety key

--- sEleitoral/sEleitoral/settings.py
+++ sEleitoral/sEleitoral/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hikfilhsy#@!ty0d(c64dnb^6y(ywv=l9wm(49-lg2hnbj939+'
+SECRET_KEY = 'ADICIONE_SUA_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
